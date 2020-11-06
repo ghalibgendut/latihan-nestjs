cd6e8d5973f37ab28430922c3dfbce9282acf455
unknown <ghalibsasmito@gmail.com>
2020-11-06 15:45:49 +0700

    Latihan nest js

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('coba')
+  getHi(): string {
+    return this.appService.getHello();
+  }
 }
